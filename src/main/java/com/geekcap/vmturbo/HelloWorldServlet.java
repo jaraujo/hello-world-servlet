eb48ddc8d278bf201d5d0d1ad98284da39e7efed
jaraujo <inbox4joe@outlook.com>
2018-03-13 21:03:34 -0700

    Update HelloWorldServlet.java

--- src/main/java/com/geekcap/vmturbo/HelloWorldServlet.java
+++ src/main/java/com/geekcap/vmturbo/HelloWorldServlet.java
@@ -1,14 +1,23 @@
-package com.geekcap.vmturbo;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import java.io.*;
-
-public class HelloWorldServlet extends HttpServlet 
-{
-  public void service( HttpServletRequest req, HttpServletResponse res ) throws IOException {
-    PrintWriter out = res.getWriter();
-    out.println( "Hello, To Joe!" );
-    out.close();
-  }
+public class HelloWorldServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest reqest, HttpServletResponse response) 
+			throws ServletException, IOException {
+		response.getWriter().println("Hello World!");
+	}
+	@Override
+	public void init() throws ServletException {
+		System.out.println("Servlet " + this.getServletName() + " has started");
+	}
+	@Override
+	public void destroy() {
+		System.out.println("Servlet " + this.getServletName() + " has stopped");
+	}
 }
+
